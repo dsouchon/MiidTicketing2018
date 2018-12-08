18808b11e5e666d3153874f4a30baebfa9109086
Donavan <Donavanwallis@hotmail.com>
2018-12-08 20:05:34 +0200

    commit
    commit

--- AndroidTicketApp/app/src/main/java/com/example/dsouchon/myapplication/MySOAPCallActivity.java
+++ AndroidTicketApp/app/src/main/java/com/example/dsouchon/myapplication/MySOAPCallActivity.java
@@ -52,8 +52,7 @@ public class MySOAPCallActivity
    //public  final String SOAP_ADDRESS = "http://www.miid.co.zw/miidwebservice.asmx";
    // public  final String SOAP_ADDRESS = "https://www.miid.co.za/miidwebservice.asmx";
    // public  final String SOAP_ADDRESS = "http://demo.miid.co.za/miidwebservice.asmx";
-    public  final String SOAP_ADDRESS = "https://www.miid.co.za/miidwebservice.asmx";
-
+    public  final String SOAP_ADDRESS = "http://MiidService.miid.co.za/miidwebservice.asmx";
 
     public MySOAPCallActivity()
     {
